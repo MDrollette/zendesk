2a355722491b8acc3828d4df14c65d3ee10a5b21
Matt Drollette <matt@drollette.com>
2015-08-11 14:30:23 -0500

    updating tickets

--- tickets.go
+++ tickets.go
@@ -8,31 +8,31 @@ import (
 )
 
 type Ticket struct {
-	Id                  int           `json:"id,omitempty"`
-	Url                 string        `json:"url,omitempty"`
-	ExternalId          string        `json:"external_id,omitempty"`
-	CreatedAt           string        `json:"created_at,omitempty"`
-	UpdatedAt           string        `json:"updated_at,omitempty"`
-	Type                string        `json:"type,omitempty"`
-	Subject             string        `json:"subject,omitempty"`
-	RawSubject          string        `json:"raw_subject,omitempty"`
-	Description         string        `json:"description,omitempty"`
-	Priority            string        `json:"priority,omitempty"`
-	Status              string        `json:"status,omitempty"`
-	Recipient           string        `json:"recipient,omitempty"`
-	RequesterId         int           `json:"requester_id,omitempty"`
-	SubmitterId         int           `json:"submitter_id,omitempty"`
-	AssigneeId          int           `json:"assignee_id,omitempty"`
-	OrganizationId      int           `json:"organization_id,omitempty"`
-	GroupId             int           `json:"group_id,omitempty"`
-	CollaboratorIds     []int         `json:"collaborator_ids,omitempty"`
-	SharingAgreementIds []int         `json:"sharing_agreement_ids,omitempty"`
-	ForumTopicId        int           `json:"forum_topic_id,omitempty"`
-	ProblemId           int           `json:"problem_id,omitempty"`
-	HasIncidents        bool          `json:"has_incidents,omitempty"`
-	DueAt               string        `json:"due_at,omitempty"`
-	Tags                []string      `json:"tags,omitempty"`
-	CustomFields        []CustomField `json:"custom_fields,omitempty"`
+	Id                  int            `json:"id,omitempty"`
+	Url                 string         `json:"url,omitempty"`
+	ExternalId          string         `json:"external_id,omitempty"`
+	CreatedAt           string         `json:"created_at,omitempty"`
+	UpdatedAt           string         `json:"updated_at,omitempty"`
+	Type                string         `json:"type,omitempty"`
+	Subject             string         `json:"subject,omitempty"`
+	RawSubject          string         `json:"raw_subject,omitempty"`
+	Description         string         `json:"description,omitempty"`
+	Priority            string         `json:"priority,omitempty"`
+	Status              string         `json:"status,omitempty"`
+	Recipient           string         `json:"recipient,omitempty"`
+	RequesterId         int            `json:"requester_id,omitempty"`
+	SubmitterId         int            `json:"submitter_id,omitempty"`
+	AssigneeId          int            `json:"assignee_id,omitempty"`
+	OrganizationId      int            `json:"organization_id,omitempty"`
+	GroupId             int            `json:"group_id,omitempty"`
+	CollaboratorIds     []int          `json:"collaborator_ids,omitempty"`
+	SharingAgreementIds []int          `json:"sharing_agreement_ids,omitempty"`
+	ForumTopicId        int            `json:"forum_topic_id,omitempty"`
+	ProblemId           int            `json:"problem_id,omitempty"`
+	HasIncidents        bool           `json:"has_incidents,omitempty"`
+	DueAt               string         `json:"due_at,omitempty"`
+	Tags                []string       `json:"tags,omitempty"`
+	CustomFields        []*CustomField `json:"custom_fields,omitempty"`
 }
 
 type CustomField struct {
@@ -100,6 +100,18 @@ func (api *TicketApi) getTicket(path string, params *url.Values) (Ticket, error)
 	return response.Ticket, nil
 }
 
+func (api *TicketApi) putTicket(path string, payload interface{}) (Ticket, error) {
+	response := struct {
+		Ticket Ticket `json:"ticket"`
+	}{}
+
+	err := api.client.put(api.context, path, payload, &response)
+	if err != nil {
+		return Ticket{}, err
+	}
+	return response.Ticket, nil
+}
+
 func (api *TicketApi) List() ([]Ticket, error) {
 	return api.getTickets("/api/v2/tickets.json", nil)
 }
@@ -107,4 +119,9 @@ func (api *TicketApi) List() ([]Ticket, error) {
 func (api *TicketApi) Show(id int) (Ticket, error) {
 	path := fmt.Sprintf("/api/v2/tickets/%d.json", id)
 	return api.getTicket(path, nil)
+}
+
+func (api *TicketApi) UpdateTicket(ticket Ticket) (Ticket, error) {
+	path := fmt.Sprintf("/api/v2/tickets/%d.json", ticket.Id)
+	return api.putTicket(path, map[string]Ticket{"ticket": ticket})
 }

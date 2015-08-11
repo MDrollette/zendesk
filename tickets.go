eb0f3b50600801e529c98c2a93ef6332ad9d7a55
Matt Drollette <matt@drollette.com>
2015-08-11 10:51:13 -0500

    ticket custom fields

--- tickets.go
+++ tickets.go
@@ -8,52 +8,60 @@ import (
 )
 
 type Ticket struct {
-	Id              int    `json:"id,omitempty"`
-	Url             string `json:"url,omitempty"`
-	ExternalId      string `json:"external_id,omitempty"`
-	CreatedAt       string `json:"created_at,omitempty"`
-	UpdatedAt       string `json:"updated_at,omitempty"`
-	Type            string `json:"type,omitempty"`
-	Subject         string `json:"subject,omitempty"`
-	RawSubject      string `json:"raw_subject,omitempty"`
-	Description     string `json:"description,omitempty"`
-	Priority        string `json:"priority,omitempty"`
-	Status          string `json:"status,omitempty"`
-	Recipient       string `json:"recipient,omitempty"`
-	RequesterId     int    `json:"requester_id,omitempty"`
-	SubmitterId     int    `json:"submitter_id,omitempty"`
-	AssigneeId      int    `json:"assignee_id,omitempty"`
-	OrganizationId  int    `json:"organization_id,omitempty"`
-	GroupId         int    `json:"group_id,omitempty"`
-	CollaboratorIds []int  `json:"collaborator_ids,omitempty"`
-	ForumTopicId    int    `json:"forum_topic_id,omitempty"`
-	ProblemId       int    `json:"problem_id,omitempty"`
+	Id                  int           `json:"id,omitempty"`
+	Url                 string        `json:"url,omitempty"`
+	ExternalId          string        `json:"external_id,omitempty"`
+	CreatedAt           string        `json:"created_at,omitempty"`
+	UpdatedAt           string        `json:"updated_at,omitempty"`
+	Type                string        `json:"type,omitempty"`
+	Subject             string        `json:"subject,omitempty"`
+	RawSubject          string        `json:"raw_subject,omitempty"`
+	Description         string        `json:"description,omitempty"`
+	Priority            string        `json:"priority,omitempty"`
+	Status              string        `json:"status,omitempty"`
+	Recipient           string        `json:"recipient,omitempty"`
+	RequesterId         int           `json:"requester_id,omitempty"`
+	SubmitterId         int           `json:"submitter_id,omitempty"`
+	AssigneeId          int           `json:"assignee_id,omitempty"`
+	OrganizationId      int           `json:"organization_id,omitempty"`
+	GroupId             int           `json:"group_id,omitempty"`
+	CollaboratorIds     []int         `json:"collaborator_ids,omitempty"`
+	SharingAgreementIds []int         `json:"sharing_agreement_ids,omitempty"`
+	ForumTopicId        int           `json:"forum_topic_id,omitempty"`
+	ProblemId           int           `json:"problem_id,omitempty"`
+	HasIncidents        bool          `json:"has_incidents,omitempty"`
+	DueAt               string        `json:"due_at,omitempty"`
+	Tags                []string      `json:"tags,omitempty"`
+	CustomFields        []CustomField `json:"custom_fields,omitempty"`
+}
 
-	HasIncidents bool     `json:"has_incidents,omitempty"`
-	DueAt        string   `json:"due_at,omitempty"`
-	Tags         []string `json:"tags,omitempty"`
+type CustomField struct {
+	Id    int         `json:"id"`
+	Value interface{} `json:"value"`
+}
 
-	//   "via": {
-	//     "channel": "web"
-	//   },
-	//   "custom_fields": [
-	//     {
-	//       "id":    27642,
-	//       "value": "745"
-	//     },
-	//     {
-	//       "id":    27648,
-	//       "value": "yes"
-	//     }
-	//   ],
-	//   "satisfaction_rating": {
-	//     "id": 1234,
-	//     "score": "good",
-	//     "comment": "Great support!"
-	//   },
-	//   "sharing_agreement_ids": [84432]
-	// }
+func (cf *CustomField) StringVal() string {
+	if s, ok := cf.Value.(string); ok {
+		return s
+	}
 
+	return ""
+}
+
+func (cf *CustomField) IntVal() int64 {
+	if i, ok := cf.Value.(int64); ok {
+		return i
+	}
+
+	return int64(0)
+}
+
+func (cf *CustomField) BoolVal() bool {
+	if b, ok := cf.Value.(bool); ok {
+		return b
+	}
+
+	return false
 }
 
 type TicketApi struct {
